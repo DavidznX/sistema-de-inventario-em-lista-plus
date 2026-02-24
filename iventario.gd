a2ba2b91ba010ac98be3314fad50f74c76d9949e
David Gabriel Lacerda Dutra <davidlacerdadutra123@gmail.com>
2026-02-23 21:22:19 -0300

    .

--- iventario.gd
+++ iventario.gd
@@ -4,12 +4,11 @@ var inventario = []
 
 @onready var lista_de_itens: VBoxContainer = $lista_de_itens
 
-var SlotScene = preload("res://slot.tscn")
+
 
 func guardar_item(item):
 	inventario.append(item)
-	
-	var slot = SlotScene.instantiate()
-	slot.nome_item.text = item.nome
-	
-	lista_de_itens.add_child(slot)
+	return item
+
+func listar_os_itens_no_invetario():
+	pass
